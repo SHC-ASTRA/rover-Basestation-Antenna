f1f680ca78613c97ad7052d8d7873874ead00e0b
David Sharpe <ds0196@uah.edu>
2025-05-10 22:33:38 -0500

    add initial code

---
+++ antenna_main/include/AntennaMainMCU.h
@@ -0,0 +1,15 @@
+/**
+ * @file AntennaMainMCU.h
+ * @author your name (you@domain.com)
+ * @brief
+ *
+ */
+#pragma once
+
+
+#ifdef ARDUINO_ADAFRUIT_FEATHER_ESP32_V2
+
+#define ETHERNET_CS 33
+
+
+#endif
--- antenna_main/src/main.cpp
+++ antenna_main/src/main.cpp
@@ -1,18 +1,225 @@
-#include <Arduino.h>
+/**
+ * @file main.cpp
+ * @author David Sharpe (ds0196@uah.edu)
+ * @brief description
+ *
+ */
 
-// put function declarations here:
-int myFunction(int, int);
+//------------//
+//  Includes  //
+//------------//
 
+#include "AstraMisc.h"
+#include "AntennaMainMCU.h"
+#include <SPI.h>
+#include <Ethernet.h>
+#include <LSS.h>
+
+
+//------------//
+//  Settings  //
+//------------//
+
+// Comment out to disable LED blinking
+#define BLINK
+
+// Enter a MAC address and IP address for your controller below.
+// The IP address will be dependent on your local network:
+byte mac[] = {
+    0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
+};
+IPAddress ip(192, 168, 1, 177);
+unsigned int localPort = 8888;  // local port to listen on
+// An EthernetUDP instance to let us send and receive packets over UDP
+EthernetUDP Udp;
+
+
+//---------------------//
+//  Component classes  //
+//---------------------//
+
+LSS myLSS(0);
+
+// Adafruit_BNO055 bno;
+
+
+//----------//
+//  Timing  //
+//----------//
+
+uint32_t lastBlink = 0;
+bool ledState = false;
+
+
+//--------------//
+//  Prototypes  //
+//--------------//
+
+
+//------------------------------------------------------------------------------------------------//
+//  Setup
+//------------------------------------------------------------------------------------------------//
+//
+//
+//------------------------------------------------//
+//                                                //
+//      ////////    //////////    //////////      //
+//    //                //        //        //    //
+//    //                //        //        //    //
+//      //////          //        //////////      //
+//            //        //        //              //
+//            //        //        //              //
+//    ////////          //        //              //
+//                                                //
+//------------------------------------------------//
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    //--------//
+    //  Pins  //
+    //--------//
+
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(1000);
+    digitalWrite(LED_BUILTIN, LOW);
+
+
+    //------------------//
+    //  Communications  //
+    //------------------//
+
+    Serial.begin(SERIAL_BAUD);
+    Serial1.begin(LSS_BAUD);
+
+    Ethernet.init(ETHERNET_CS);
+    Ethernet.begin(mac, ip);
+
+
+    //-----------//
+    //  Sensors  //
+    //-----------//
+
+
+    //--------------------//
+    //  Misc. Components  //
+    //--------------------//
+
+    // Check for Ethernet hardware present
+    if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+        Serial.println("Ethernet shield was not found.");
+    }
+    if (Ethernet.linkStatus() == LinkOFF) {
+        Serial.println("Ethernet cable is not connected.");
+    }
+
+    Udp.begin(localPort);
 }
 
+
+//------------------------------------------------------------------------------------------------//
+//  Loop
+//------------------------------------------------------------------------------------------------//
+//
+//
+//-------------------------------------------------//
+//                                                 //
+//    /////////      //            //////////      //
+//    //      //     //            //        //    //
+//    //      //     //            //        //    //
+//    ////////       //            //////////      //
+//    //      //     //            //              //
+//    //       //    //            //              //
+//    /////////      //////////    //              //
+//                                                 //
+//-------------------------------------------------//
 void loop() {
-  // put your main code here, to run repeatedly:
+    //----------//
+    //  Timers  //
+    //----------//
+#ifdef BLINK
+    if (millis() - lastBlink > 1000) {
+        lastBlink = millis();
+        ledState = !ledState;
+        digitalWrite(LED_BUILTIN, ledState);
+    }
+#endif
+
+
+    //-------------//
+    //  CAN Input  //
+    //-------------//
+
+
+    //------------------//
+    //  UART/USB Input  //
+    //------------------//
+    //
+    //
+    //-------------------------------------------------------//
+    //                                                       //
+    //      /////////    //\\        ////    //////////      //
+    //    //             //  \\    //  //    //        //    //
+    //    //             //    \\//    //    //        //    //
+    //    //             //            //    //        //    //
+    //    //             //            //    //        //    //
+    //    //             //            //    //        //    //
+    //      /////////    //            //    //////////      //
+    //                                                       //
+    //-------------------------------------------------------//
+    if (Serial.available()) {
+        String input = Serial.readStringUntil('\n');
+
+        input.trim();                   // Remove preceding and trailing whitespace
+        std::vector<String> args = {};  // Initialize empty vector to hold separated arguments
+        parseInput(input, args);   // Separate `input` by commas and place into args vector
+        args[0].toLowerCase();          // Make command case-insensitive
+        String command = args[0];       // To make processing code more readable
+
+        //--------//
+        //  Misc  //
+        //--------//
+        if (command == "ping") {
+            Serial.println("pong");
+        }
+
+        else if (command == "time") {
+            Serial.println(millis());
+        }
+
+        else if (command == "led") {
+            if (args[1] == "on")
+                digitalWrite(LED_BUILTIN, HIGH);
+            else if (args[1] == "off")
+                digitalWrite(LED_BUILTIN, LOW);
+            else if (args[1] == "toggle") {
+                ledState = !ledState;
+                digitalWrite(LED_BUILTIN, ledState);
+            }
+        }
+
+        //-----------//
+        //  Sensors  //
+        //-----------//
+
+        //----------//
+        //  Motors  //
+        //----------//
+    }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+
+//------------------------------------------------------------------------------------------------//
+//  Function definitions
+//------------------------------------------------------------------------------------------------//
+//
+//
+//----------------------------------------------------//
+//                                                    //
+//    //////////    //          //      //////////    //
+//    //            //\\        //    //              //
+//    //            //  \\      //    //              //
+//    //////        //    \\    //    //              //
+//    //            //      \\  //    //              //
+//    //            //        \\//    //              //
+//    //            //          //      //////////    //
+//                                                    //
+//----------------------------------------------------//
